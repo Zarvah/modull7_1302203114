cb589449d52aad23499599516acdbd7f7e6a832a
Zarvah <mznauval.priadi@gmail.com>
2022-04-09 18:15:26 +0700

    aplikasi bank transfer dengan bank configuration

---
+++ modull7_1302203114/bank_transfer_config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modull7_1302203114
+{
+    internal class bank_transfer_config
+    {
+    }
+}
